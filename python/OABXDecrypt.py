fb27cc176739b99361b2ed666cd2d9cc1226c342
Kevin M <sylikc@gmail.com>
2024-01-24 13:29:31 -0800

    add a bulk mode with custom output dir.  This permits running against a whole dir and decrypting everything

--- python/OABXDecrypt.py
+++ python/OABXDecrypt.py
@@ -85,8 +85,9 @@ OABX Decrypt (NeoBackup Decrypt)
 Reference Python implementation
 
 Examples:
-  {f}
-  {f}
+  {f} --file somewhere/data.tar.gz.enc --propfile somewhereelse/user.properties --passfile filewithpass.txt
+  {f} --file somewhere/data.tar.gz.enc --propfile somewhereelse/user.properties --output /tmp
+  {f} --bulk backupdir/copied/from/storage --output /tmp --passfile filewithpass.txt
 
 Errorlevel:
   0 = success
@@ -96,17 +97,23 @@ Errorlevel:
     parser = argparse.ArgumentParser(description=help_text, formatter_class=argparse.RawTextHelpFormatter)
 
     # rudimentary creation of args, without getting too fancy with groups
+    # correct param arguments are checked later, without enforcing complex required combinations with argparse
+
     parser.add_argument("-f", "--file",
                         type=str,
-                        metavar="FILE",
-                        required=True,
+                        metavar="ENCRYPTED_FILE",
                         help="path/to/encrypted/backup.tar.gz.enc")
     parser.add_argument("-p", "--propfile",
                         type=str,
                         metavar="PROPERTIES_FILE",
-                        required=True,
                         help="path/to/propfile.properties")
 
+
+
+    parser.add_argument("--bulk",
+                        type=str,
+                        metavar="BASE_DIR",
+                        help="Base dir to start bulk operation (unmodified NeoBackup structure), find all .properties files and decrypt all *.enc")
 
 
     parser.add_argument("--passfile",
@@ -118,20 +125,15 @@ Errorlevel:
                         help="Specify a password on command line (insecure, not recommended).  By default, will read NB_PASSWORD environment variable for password")
 
 
-
-
     parser.add_argument("-o", "--output", type=str, metavar="PATH", help="Output directory.  Default is same base path as --file")
+
+
 
 
 
     # parse command line
     args = parser.parse_args()
 
-
-    # does not error check paths for existence yet
-    encfile = Path(args.file)
-    propfile = Path(args.propfile)
-    outfile = encfile.with_suffix("")
 
     # get argument or environment variable
     if args.passfile:
@@ -144,10 +146,76 @@ Errorlevel:
         # turn into bytes
         password_b = password.encode()
 
-    oabxdecrypt(encfile,
-                propfile,
-                outfile,
-                password=password_b)
+
+
+    if args.file and args.propfile and not args.bulk:
+        # valid combo for running on single file
+
+        # does not error check paths for existence yet
+        encfile = Path(args.file)
+        propfile = Path(args.propfile)
+        outfile = encfile.with_suffix("")
+
+        if args.output:
+            # replace with the proper output path for the file
+            outfile = Path(args.output) / outfile.name
+
+        print(f"enc file:  {encfile}")
+        print(f"prop file: {propfile}")
+        print(f"out file:  {outfile}")
+
+        oabxdecrypt(encfile,
+                    propfile,
+                    outfile,
+                    password=password_b)
+
+    elif args.bulk and not (args.file or args.propfile):
+        # search and decrypt all, crashes on first bad decryption
+        bulk_path = Path(args.bulk)
+
+        if args.output:
+            output_path = Path(args.output)
+            # must exist
+            if not output_path.is_dir():
+                raise NotADirectoryError
+        else:
+            output_path = None
+
+
+        if not bulk_path.is_dir():
+            raise NotADirectoryError
+
+        # recursive search through the bulk_path
+        for propfile in bulk_path.glob("**/*.properties"):
+            # properties file found, search for encrypted files at that level
+
+            # the directory is named the same as the properties file with ".properties"
+            # only one flat level of files
+            for encfile in propfile.with_suffix("").glob("*.enc"):
+                # remove .enc for output file
+                outfile = encfile.with_suffix("")
+
+                if output_path:
+                    # replace with output path in the same relative path in basedir
+                    relative = outfile.relative_to(bulk_path)
+                    outfile = output_path / relative
+
+                    # create the subdir structure for custom output
+                    outfile.parent.mkdir(parents=True, exist_ok=True)
+
+                print(f"enc file:  {encfile}")
+                print(f"prop file: {propfile}")
+                print(f"out file:  {outfile}")
+
+                oabxdecrypt(encfile,
+                            propfile,
+                            outfile,
+                            password=password_b)
+
+
+    else:
+        raise ValueError("Invalid combination of parameters")
+
 
     # MAC check failed means a bad file or wrong password, etc
 
